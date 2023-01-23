174f1e3babd8f72ee0cf7c04a5cfecb8c666514f
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-23 20:40:48 +0500

    Completed task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 19
+//
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//
+//14212 -> нет
+//
+//12821 -> да
+//
+//23432 -> да
+
+System.Console.Write("Введите число: ");
+string inputValue = System.Console.ReadLine();
+
+for (int i = 0; i < inputValue.Length; i++) {
+    if(inputValue[i] != inputValue[inputValue.Length - 1 - i]) {
+        System.Console.WriteLine("Число не палиндром!");
+        break;
+    }
+    
+    if(i == inputValue.Length - 1) {
+        System.Console.WriteLine("Число палиндром");
+        break;
+    }
+}
+
